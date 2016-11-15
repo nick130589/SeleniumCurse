147d947d622d0de6d5c2f69877b9fea31e79a4c3
nick130589 <nick13.05@i.ua>
2016-11-15 09:10:14 +0200

    Task_5 was implemented

--- Selenium-Test/src/com/example/fw/ApplicationManager.java
+++ Selenium-Test/src/com/example/fw/ApplicationManager.java
@@ -2,7 +2,10 @@ package com.example.fw;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.io.File;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,12 +19,26 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private Properties properties;
 
-    public ApplicationManager() {
-        driver = new FirefoxDriver();
-        baseUrl = "http://localhost/";
+    public ApplicationManager(Properties properties) {
+        this.properties = properties;
+        String browser = properties.getProperty("browser");
+        if ("firefox".equals(browser)) {
+            driver = new FirefoxDriver();
+        } else if ("ie".equals(browser))
+        {
+            File file = new File("D:\\Selenium_web_driver\\IEDriverServer_Win32_2.53.1\\IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+            driver = new InternetExplorerDriver();
+        } else
+        {
+            throw new Error("Unsupported browser: " + browser);
+        }
+
+        baseUrl = properties.getProperty("baseUrl");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get(baseUrl + "/addressbookv4.1.4/");
+        driver.get(baseUrl);
     }
 
     public void stop() {
--- Selenium-Test/src/com/example/tests/ContactCreationTests.java
+++ Selenium-Test/src/com/example/tests/ContactCreationTests.java
@@ -2,15 +2,28 @@ package com.example.tests;
 
 
 import com.example.utils.SortedListOf;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import static com.example.tests.ContactDataGenerator.loadContactsFromXmlFile;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 
 public class ContactCreationTests extends TestBase {
 
-    @Test(dataProvider = "randomValidContactGenerator")
+    @DataProvider
+    public Iterator<Object[]> contactsFromFile() throws IOException {
+        //return wrapContactForDataProvider(loadContactsFromCsvFile(new File("contacts.txt"))).iterator();
+        return wrapContactForDataProvider(loadContactsFromXmlFile(new File("contacts.xml"))).iterator();
+    }
+
+
+    @Test(dataProvider = "contactsFromFile")
     public void testContactCreationWithValidData(ContactData contact) throws Exception {
         //save old state
         SortedListOf<ContactData> oldList = app.getContactHelper().getContactData();
