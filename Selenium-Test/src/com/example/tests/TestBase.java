e8ea1d78e43444e33ce931560963aefaa85252cb
nick130589 <nick13.05@i.ua>
2016-12-03 19:55:09 +0200

    Task_5 was changed

--- Selenium-Test/src/com/example/tests/TestBase.java
+++ Selenium-Test/src/com/example/tests/TestBase.java
@@ -1,16 +1,19 @@
 package com.example.tests;
 
 import com.example.fw.ApplicationManager;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
 
-import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 import static com.example.tests.ContactDataGenerator.generateRandomContatcts;
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 
 /**
  * Created by Nick on 9/17/2016.
@@ -19,14 +22,15 @@ public class TestBase {
 
     protected ApplicationManager app;
 
-    @BeforeSuite
+    @BeforeTest
     public void setUp() throws Exception {
+        String configFile = System.getProperty("configFile", "application.properties");
         Properties properties = new Properties();
-        properties.load(new FileReader(new File("D:\\Repositoriy\\SeleniumCurse\\Selenium-Test\\src\\application.properties")));
+        properties.load(new FileReader(new File(configFile)));
         app = new ApplicationManager(properties);
     }
 
-    @AfterSuite
+    @AfterTest
     public void tearDown() throws Exception {
         app.stop();
     }
